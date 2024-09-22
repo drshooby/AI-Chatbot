46db5d6ab687efb76ee64d54198ad3ce4847e898
David Shubov <116600844+shooby-d@users.noreply.github.com>
2024-09-22 16:42:12 -0700

    adding files

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("script connected")
+
+const inputField = document.getElementById('user-input')
+const sendBtn =  document.getElementById('send-btn')
+const messagesContainer = document.getElementById('messages')
+
+let inputText = ""
+
+inputField.addEventListener('input', () => {
+    inputText = inputField.value
+})
+
+inputField.addEventListener('keydown', (keyEvent) => {
+    if (keyEvent.key == 'Enter') {
+        sendMessage()
+    }
+})
+
+sendBtn.addEventListener('click', sendMessage)
+
+function sendMessage() {
+    inputText = inputText.trim()
+    if (inputText) {
+        messagesContainer.innerText = inputText
+    } else {
+        alert("Empty message body")
+    }
+    inputField.value = ''
+}
